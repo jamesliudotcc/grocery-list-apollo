d829edb2d183191e25a7cb0e988df190d38332f1
jamesliudotcc <james@jamesliu.cc>
2019-02-27 09:54:32 -0800

    Sending auth as header, probably

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import ApolloClient from 'apollo-boost';
+import { setContext } from 'apollo-link-context';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
+import { AUTH_TOKEN } from './constants';
 
 const GRAPHQL_API_URL = 'http://localhost:8000/graphql/';
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem(AUTH_TOKEN);
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
 const client = new ApolloClient({
   clientState: {
@@ -19,6 +31,14 @@ const client = new ApolloClient({
     },
   },
   uri: GRAPHQL_API_URL,
+  request: async operation => {
+    const token = await localStorage.getItem(AUTH_TOKEN);
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
 });
 
 render(
